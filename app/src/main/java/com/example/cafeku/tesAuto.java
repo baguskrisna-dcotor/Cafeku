1672a96b3b863046f934dd6a272395ee6e66009c
SSSoftBerafo <andro9286@gmail.com>
2025-09-29 08:44:43 +0700

    voucherpg

--- app/src/main/java/com/example/cafeku/tesAuto.java
+++ app/src/main/java/com/example/cafeku/tesAuto.java
@@ -22,7 +22,6 @@ public class tesAuto extends AppCompatActivity {
     int[] txtIds1 = {R.id.txtMenu1, R.id.txtMenu2, R.id.txtMenu3, R.id.txtMenu4, R.id.txtMenu5};
     int[] btnIds1 = {R.id.btnMenu1, R.id.btnMenu2, R.id.btnMenu3, R.id.btnMenu4, R.id.btnMenu5};
 
-    // Data produk
     int[] images1 = {R.drawable.menu_1, R.drawable.menu_2, R.drawable.menu_3, R.drawable.menu_5, R.drawable.menu_6};
     String[] names1 = {"Kentang Goreng", "Burger", "Sosis Bakar", "Pizza", "Nasi Goreng"};
     String[] prices1 = {"Rp 15.000", "Rp 20.000", "Rp 10.000", "Rp 30.000", "Rp 25.000"};
@@ -32,7 +31,6 @@ public class tesAuto extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tes_auto);
         tambahProduk(parent1, imgIds1, txtIds1, btnIds1, images1, names1, prices1);
-
     }
     public void tambahProduk(int[] katalogId, int[] imgId, int[] txtId, int[] btnId, int[] image, String[] name, String[] price){
         for (int i = 0; i < name.length; i++) {
@@ -43,12 +41,10 @@ public class tesAuto extends AppCompatActivity {
             MaterialButton btn = findViewById(btnId[i]);
             LinearLayout katalog = findViewById(katalogId[i]);
 
-            // Set data
             img.setImageResource(image[i]);
             txt.setText(name[i]);
             btn.setText(price[i]);
 
-            // OnClick katalog → pindah ke detail
             katalog.setOnClickListener(v -> {
                 Intent intent = new Intent(tesAuto.this, DetailActivity.class);
                 intent.putExtra("img", image[index]);
